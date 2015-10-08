51754fb0874555f4d9684b3baeafdf369e18334e
vogrelord <mail@vogre.ru>
2015-10-09 01:58:15 +0300

    added calendars to default route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var calendar = require('../calendars')['default'];
+var calendars = require('../calendars');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {route:'default', title: 'Express', calendar: calendar });
+  res.render('index', {route:'default', title: 'Express', calendar: calendar, calendars: calendars });
 });
 
 module.exports = router;
